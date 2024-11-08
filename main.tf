b62d0ae4fe14e9a26f3e06e2f4fab74c622fd619
Maxwell Ehiorobo <44559113+maxehio@users.noreply.github.com>
2024-11-08 10:22:17 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"  # Use any region; this is just for bypassing the error
+}
+
+provider "null" {}
+
+resource "null_resource" "example" {}
+
+output "stack_b_message" {
+  value = "This is Stack B's output"
+}
